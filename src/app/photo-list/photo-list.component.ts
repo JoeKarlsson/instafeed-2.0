c2d88aac49966c4631e1188f2dab52bda4cbf489
Joe Karlsson <joe.carlson@callmejoe.net>
2016-08-11 09:51:50 -1000

    gird state functional

--- src/app/photo-list/photo-list.component.ts
+++ src/app/photo-list/photo-list.component.ts
@@ -14,12 +14,24 @@ export class PhotoListComponent implements OnInit {
   private errorMessage: string;
   public photos: Array<any>;
   private instaAPI: InstaAPIService;
+  private grid: Boolean;
 
   constructor( instaAPI: InstaAPIService) {
     this.instaAPI = instaAPI
   }
 
+  onSetGrid(e) {
+    this.grid = true;
+    console.log('this.grid: ', this.grid);
+  }
+
+  onSetList(e) {
+    this.grid = false;
+    console.log('this.grid: ', this.grid);
+  }
+
   ngOnInit() {
+    this.grid = true;
     this.getInstaPhotos();
   }
 
